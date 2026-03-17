2c039fec37eefd4ea3512e5cf50ab8ce91ad5082
Isac <isaacalvarez0516@gmail.com>
2026-03-17 15:59:25 -0500

    inventario listo

--- funciones.py
+++ funciones.py
@@ -1,5 +1,9 @@
 from colores import *
+
 inventario = []
+
+inventario = []
+
 
 def limpiar_pantalla():
     import os
@@ -11,3 +15,30 @@ def menu():
     print(f"2. {VERDE}Mostrar inventario{BLANCO}")
     print(f"3. {VERDE}Calcular estadisticas{BLANCO}")
     print(f"4. {ROJO}Salir{BLANCO}")
+    
+def pedir_producto():
+    limpiar_pantalla()
+    producto = input("\nIngrese el nombre del producto:\n# ")
+    limpiar_pantalla()
+    precio = float(input("\nIngrese el precio del producto:\n# "))
+    limpiar_pantalla()
+    cantidad = int(input("\nIngrese la cantidad del producto:\n# "))
+            
+    nuevo_producto = {"nombre": producto, "precio": precio, "cantidad": cantidad}
+    inventario.append(nuevo_producto)
+            
+    print("Producto agregado con éxito")
+    input("Presione enter para continuar")
+    limpiar_pantalla()
+
+def mostrar_inventario():
+    limpiar_pantalla()
+    print("\nInventario:\n")
+    for i, producto in enumerate(inventario, 1):
+        print(f"{i}. Producto: {producto['nombre']} | Precio: {producto['precio']} | Cantidad: {producto['cantidad']}")
+    input("\nPresione enter para continuar")
+    limpiar_pantalla()
+
+def salir():
+    continuar = input("¿Está seguro de que desea salir? (si/no): ")
+    limpiar_pantalla()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from funciones import *
 from colores import *
 
+continuar = "no"
 limpiar_pantalla()
 
 
-continuar = "no"
-inventario = []
+
 
 while continuar == "no":
     try:
@@ -13,28 +13,10 @@ while continuar == "no":
         opcion = int(input("\nIngrese una opción:\n# "))
 
         if opcion == 1:
-            limpiar_pantalla()
-            producto = input("\nIngrese el nombre del producto:\n# ")
-            limpiar_pantalla()
-            precio = float(input("\nIngrese el precio del producto:\n# "))
-            limpiar_pantalla()
-            cantidad = int(input("\nIngrese la cantidad del producto:\n# "))
+            pedir_producto()
             
-            nuevo_producto = {"nombre": producto, "precio": precio, "cantidad": cantidad}
-            inventario.append(nuevo_producto)
-            
-            print("Producto agregado con éxito")
-            input("Presione enter para continuar")
-            limpiar_pantalla()
-
         elif opcion == 2:
-            limpiar_pantalla()
-            print("\nInventario:\n")
-            for i, producto in enumerate(inventario, 1):
-                print(f"{i}. Producto: {producto['nombre']} | Precio: {producto['precio']} | Cantidad: {producto['cantidad']}")
-            input("\nPresione enter para continuar")
-            limpiar_pantalla()
-
+            mostrar_inventario()
 
         elif opcion == 3:
             print()
